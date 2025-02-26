d6bba2516a0084f95e7353d2a34f8892756da4e9
Jacqueline <jacquelinec171@gmail.com>
2025-02-25 23:55:01 -0500

    Update cis4340a.java

--- cis4340a.java
+++ cis4340a.java
@@ -8,7 +8,7 @@
  *
  * Revision History:
  *   % java Cis4340
- *  Hello, World - from the class of cis4340 - Spring 2024 - UCF (your name here)
+ *  Hello, World - from the class of cis4340 - Spring 2024 - UCF (Jacqueline Carroll)
  *  %
  *
  ******************************************************************************/
@@ -17,7 +17,7 @@ public class Cis4340 {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World - from the class of cis4340 - Spring 2024 - UCF (your name here)");
+        System.out.println("Hello, World - from the class of cis4340 - Spring 2024 - UCF (Jacqueline Carroll)");
     }
 
 }
