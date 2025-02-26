50db51b76dd3923731fbf88974b78366fa33ff0c
Jacqueline <jacquelinec171@gmail.com>
2025-02-26 00:57:38 -0500

    Initial upload of R04_STR03_J.java - Noncompliant example

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,6 +1,7 @@
+Rule 04. Characters and Strings (STR)
 
 BigInteger x = new BigInteger("530500452766");
-String s = x.toString();  // Valid character data
-byte[] byteArray = s.getBytes();
-String ns = new String(byteArray); 
-x = new BigInteger(ns);
+byte[] byteArray = x.toByteArray();
+String s = new String(byteArray);
+byteArray = s.getBytes();
+x = new BigInteger(byteArray);
