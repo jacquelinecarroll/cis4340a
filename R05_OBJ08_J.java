9795b9b9501e498502d2b80b130ae4b536bcc99f
Jacqueline <jacquelinec171@gmail.com>
2025-02-26 15:14:49 -0500

    Initial upload of R05_OBJ08_J.java - Noncompliant example

---
+++ R05_OBJ08_J.java
@@ -0,0 +1,20 @@
+Rule 05. Object Orientation (OBJ)
+
+class Coordinates {
+    private int x;
+    private int y;
+   
+    public class Point {
+      public void getPoint() {
+        System.out.println("(" + x + "," + y + ")");
+      }
+    }
+  }
+   
+  class AnotherClass {
+    public static void main(String[] args) {
+      Coordinates c = new Coordinates();
+      Coordinates.Point p = c.new Point();
+      p.getPoint();
+    }
+  }
