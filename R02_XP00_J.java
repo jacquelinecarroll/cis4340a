b8330e1807dec7a82bab6d7c1799804e5d037c89
Jacqueline <jacquelinec171@gmail.com>
2025-02-26 00:54:05 -0500

    Initial upload of R02_XP00_J.java - Noncompliant example

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,10 +1,8 @@
-
+Rule 02. Expressions (EXP)
 public void deleteFile(){
  
   File someFile = new File("someFileName.txt");
   // Do something with someFile
-  if (!someFile.delete()) {
-    // Handle failure to delete the file
-  }
+  someFile.delete();
  
 }
