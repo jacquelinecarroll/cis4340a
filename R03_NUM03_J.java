049fa212130c7f9f4dda8a7a443732a141349aac
Jacqueline <jacquelinec171@gmail.com>
2025-02-26 00:56:35 -0500

    Fixed upload of R03_NUM03_J.java - compliant solution

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,5 +1,5 @@
 Rule 03. Numeric Types and Operations (NUM)
 
-public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt(); 
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
