3e79b543c64c556488bfb5a49f2e29aea0c78c6c
Jacqueline <jacquelinec171@gmail.com>
2025-02-26 00:51:37 -0500

    Initial upload of R00_IDS03_J.java Noncompliant example

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,11 +1,6 @@
-
+Rule 00. Input Validation and Data Sanitization (IDS)
 if (loginSuccessful) {
-  logger.severe("User login succeeded for: " + sanitizeUser(username));
+  logger.severe("User login succeeded for: " + username);
 } else {
-  logger.severe("User login failed for: " + sanitizeUser(username));
-}
-
-public String sanitizeUser(String username) {
-  return Pattern.matches("[A-Za-z0-9_]+", username))
-      ? username : "unauthorized user";
+  logger.severe("User login failed for: " + username);
 }
